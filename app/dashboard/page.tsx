87c6cf665c5fe745ac49cb5a64bd4f3d58c62cb7
Shristi Gupta <shristiguptawork@gmail.com>
2023-10-04 00:03:40 +0530

    feat: dashboard ui placeholder created

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import StatusAlert from "@/components/ui/status-alert";
+
+const Dashboard: React.FunctionComponent = () =>{
+    return(
+        <div className="dashboard m-12">
+            <StatusAlert />
+        </div>
+    )
+}
+export default Dashboard;
