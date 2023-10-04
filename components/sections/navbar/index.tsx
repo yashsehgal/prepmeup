d1f18c3f9248e8416532c339b66e66c43f1ab4bd
Yash Sehgal <yashsehgal.work@gmail.com>
2023-10-05 02:51:38 +0530

    style: view container breakpoints

--- components/sections/navbar/index.tsx
+++ components/sections/navbar/index.tsx
@@ -1,22 +1,20 @@
 import { Button } from "@/components/ui/button";
 import { Moon } from 'lucide-react';
 import { Sun } from 'lucide-react';
-// import ViewContainer from "@/components/layouts/view-container";
+import ViewContainer from "@/components/layouts/view-container";
 
 const Navbar: React.FunctionComponent = () => {
   return (
     <nav className="navbar">
-      <section className="view-container justify-between">
-        {/* <ViewContainer className="view-container"> */}
-          <div className="font-semibold text-2xl">Welcome User</div>
-          <div className="flex gap-2">
-            <Button variant="outline">
-            <Sun/><span className="text-lg mx-1">/</span><Moon/>
-            </Button>
-            <Button className="rounded-full w-10 h-10 font-bold">YS</Button>
-          </div>
-        {/* </ViewContainer> */}
-      </section>
+      <ViewContainer className="flex">
+        <div className="font-semibold text-2xl">Welcome User</div>
+        <div className="flex gap-2">
+          <Button variant="outline">
+            <Sun /><span className="text-lg mx-1">/</span><Moon />
+          </Button>
+          <Button className="rounded-full w-10 h-10 font-bold">YS</Button>
+        </div>
+      </ViewContainer>
     </nav>
   )
 }
