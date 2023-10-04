f3d0c97c33ada0bb4ce65eb55f13e9778b6d0a17
Yash Sehgal <yashsehgal.work@gmail.com>
2023-10-05 05:12:01 +0530

    style: feature list responsive layout

--- components/sections/dashboard-features-list/index.tsx
+++ components/sections/dashboard-features-list/index.tsx
@@ -1,27 +1,28 @@
 import ViewContainer from "@/components/layouts/view-container";
 import FeatureOption from "@/components/ui/feature-option";
+import { cn } from "@/lib/utils";
 import { Briefcase, FileSearch, MessagesSquare, PencilLine, Video } from "lucide-react";
 
 const FeatureList: Array<FeatureOptionInterface> = [
   {
     featureName: "Video Interview",
-    icon: <Video />
+    icon: <Video className={cn("max-md:w-4 h-auto")} />
   },
   {
     featureName: "ATS Resume Review",
-    icon: <FileSearch />
+    icon: <FileSearch className={cn("max-md:w-4 h-auto")} />
   },
   {
     featureName: "Topic Based Q/A",
-    icon: <MessagesSquare />
+    icon: <MessagesSquare className={cn("max-md:w-4 h-auto")} />
   },
   {
     featureName: "Mock Aptitude",
-    icon: <PencilLine />
+    icon: <PencilLine className={cn("max-md:w-4 h-auto")} />
   },
   {
     featureName: "Behavioural Rounds",
-    icon: <Briefcase />
+    icon: <Briefcase className={cn("max-md:w-4 h-auto")} />
   },
 ]
 
@@ -32,7 +33,9 @@ const DashboardFeatureList: React.FunctionComponent = () => {
         <h3 className="text-lg font-semibold">
           {"Start new activity"}
         </h3>
-        <div className="features-list-container mt-3 flex flex-row gap-4 overflow-x-scroll hide-scrollbar">
+        <div className={cn("features-list-container mt-3 py-2 flex flex-row gap-4 overflow-x-scroll hide-scrollbar",
+          "max-md:grid max-md:grid-cols-2 max-md:gap-y-4 max-md:gap-x-4 max-md:overflow-visible max-md:w-fit",
+        )}>
           {FeatureList.map((featureItem, featureIndex) => (
             <FeatureOption
               featureName={featureItem.featureName}
--- components/ui/feature-option.tsx
+++ components/ui/feature-option.tsx
@@ -8,13 +8,18 @@ const FeatureOption: React.FunctionComponent<FeatureOptionInterface> = ({
   ...props
 }) => {
   return (
-    <Link href={""}>
+    <Link href={""} className="w-fit focus:outline-none focus:ring h-fit rounded-2xl max-sm:rounded-xl hover:scale-95 active:scale-90 transition-all focus:ring-offset-2 focus:ring-neutral-300">
       <div
-        className={cn("relative w-[240px] h-[140px] rounded-2xl border border-transparent bg-neutral-800 text-neutral-50 shadow hover:shadow-md transition-all")}
+        className={cn("relative w-[240px] h-[140px] rounded-2xl border border-transparent bg-gradient-to-tr from-neutral-900 to-neutral-800 text-neutral-50 shadow transition-all",
+          "max-xl:w-[220px] max-xl:h-[120px]",
+          "max-lg:w-[200px] max-lg:h-[100px]",
+          "max-sm:rounded-xl",
+          "hover:shadow-md hover:brightness-125",
+        )}
         {...props}
       >
         {icon && <div className="absolute icon-wrapper top-4 right-4">{icon}</div>}
-        {<p className="absolute font-medium text-lg bottom-4 left-4">{featureName}</p>}
+        {<p className="absolute font-medium text-lg bottom-4 left-4 max-lg:text-base">{featureName}</p>}
       </div>
     </Link>
   )
