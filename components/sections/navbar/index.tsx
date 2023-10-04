153ddfc63060ff747188921cc1b4b80c892748fc
Yash Sehgal <yashsehgal.work@gmail.com>
2023-10-05 03:15:00 +0530

    style: theme switch upgrade

--- components/sections/navbar/index.tsx
+++ components/sections/navbar/index.tsx
@@ -2,17 +2,25 @@ import { Button } from "@/components/ui/button";
 import { Moon } from 'lucide-react';
 import { Sun } from 'lucide-react';
 import ViewContainer from "@/components/layouts/view-container";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const Navbar: React.FunctionComponent = () => {
   return (
-    <nav className="navbar">
-      <ViewContainer className="flex">
-        <h1 className="font-semibold text-2xl">Welcome User</h1>
+    <nav className="navbar py-6">
+      <ViewContainer className="flex items-center justify-between">
+        <h1 className="font-semibold text-2xl">
+          {"Welcome User"}
+        </h1>
         <div className="flex gap-2">
-          <Button variant="outline">
-            <Sun /><span className="text-lg mx-1">/</span><Moon />
+          <Button variant="ghost" className="rounded-full">
+            {true ?
+              <Moon className="w-5 h-auto" />
+              : <Sun className="w-5 h-auto" />}
           </Button>
-          <Button className="rounded-full w-10 h-10 font-bold">YS</Button>
+          <Avatar>
+            <AvatarImage></AvatarImage>
+            <AvatarFallback>{"YS"}</AvatarFallback>
+          </Avatar>
         </div>
       </ViewContainer>
     </nav>
