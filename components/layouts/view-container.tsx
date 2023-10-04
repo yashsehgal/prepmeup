aa5749cea82af5009be2d1aedff3ef344cd3220c
Yash Sehgal <yashsehgal.work@gmail.com>
2023-10-05 02:37:13 +0530

    fix: view-container type / merge conflict

--- components/layouts/view-container.tsx
+++ components/layouts/view-container.tsx
@@ -1,6 +1,13 @@
-// const ViewContainer: React.FunctionComponent = ({children: any}) => {
-//     return(
-//         <div className="view-container">{children}</div>
-//     )
-// }
-// export default ViewContainer;
+import { cn } from "@/lib/utils";
+
+const ViewContainer: React.FunctionComponent<
+    React.HTMLAttributes<HTMLDivElement>
+> = ({ children, className, ...props }) => {
+    return (
+        <div className={cn("view-container", className)} {...props}>
+            {children}
+        </div>
+    )
+}
+
+export default ViewContainer;
