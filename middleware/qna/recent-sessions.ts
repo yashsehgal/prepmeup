8e7fa3863a32e70219697d034d99c947cb400294
Yash Sehgal <yashsehgal.work@gmail.com>
2023-10-10 07:08:32 +0530

    feat: add methods for handling data requests, with mock data support

---
+++ middleware/qna/recent-sessions.ts
@@ -0,0 +1,18 @@
+import { Environment } from "@/common/environment-variables";
+import { populateRecentQNASessions } from "@/mocks/qna-session-mocks";
+
+function fetchRecentQNASessions() {
+  switch (Environment.ENVIRONMENT_TYPE) {
+    case "development":
+      return populateRecentQNASessions();
+    case "production":
+      // TODO: write fetchAPI instance for returning recent sessions via firestore
+      return [];
+    default:
+      return [];
+  }
+}
+
+export {
+  fetchRecentQNASessions
+}
---
+++ mocks/qna-session-mocks.ts
@@ -0,0 +1,44 @@
+
+function populateRecentQNASessions(): RecentQNASessionCardInterface[] {
+  return [
+    {
+      lastMessageContent: "React Diffing Algorithm manages the changes",
+      title: "Discussing how react diff works?",
+      sessionID: "qna-9v38457t9857bt8"
+    },
+    {
+      lastMessageContent: "React Diffing Algorithm manages the changes",
+      title: "Discussing how react diff works?",
+      sessionID: "qna-9v38457t9857bt8"
+    },
+    {
+      lastMessageContent: "React Diffing Algorithm manages the changes",
+      title: "Discussing how react diff works?",
+      sessionID: "qna-9v38457t9857bt8"
+    },
+    {
+      lastMessageContent: "React Diffing Algorithm manages the changes",
+      title: "Discussing how react diff works?",
+      sessionID: "qna-9v38457t9857bt8"
+    },
+    {
+      lastMessageContent: "React Diffing Algorithm manages the changes",
+      title: "Discussing how react diff works?",
+      sessionID: "qna-9v38457t9857bt8"
+    },
+    {
+      lastMessageContent: "React Diffing Algorithm manages the changes",
+      title: "Discussing how react diff works?",
+      sessionID: "qna-9v38457t9857bt8"
+    },
+    {
+      lastMessageContent: "React Diffing Algorithm manages the changes",
+      title: "Discussing how react diff works?",
+      sessionID: "qna-9v38457t9857bt8"
+    },
+  ]
+}
+
+export {
+  populateRecentQNASessions
+}
