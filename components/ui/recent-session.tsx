f0dd3ecd5f6a2b604831b24ca63680eaf244c038
Shristi Gupta <shristiguptawork@gmail.com>
2023-10-12 05:18:32 +0530

    fix: Style changes

--- components/ui/recent-session.tsx
+++ components/ui/recent-session.tsx
@@ -11,11 +11,11 @@ const RecentSession: React.FunctionComponent<QNASessionCardInterface> = ({
   return (
     <Link href="#" className="w-fit focus:outline-none focus:ring h-fit rounded-2xl max-sm:rounded-xl hover:scale-95 active:scale-90 transition-all focus:ring-offset-2 focus:ring-neutral-300">
       <BoxCard
-        className={cn("bg-slate-100 to-neutral-800 text-neutral-50 hover:brightness-90"
+        className={cn("bg-neutral-100 hover:brightness-90"
         )}
       >
         <MessageSquare className={cn("absolute icon-wrapper top-4 right-4 text-black")} />
-        {<p className="absolute font-medium text-lg text-slate-300 bottom-4 left-4 max-lg:text-base">{lastMessageContent}</p>}
+        {<p className="absolute font-medium text-lg text-transparent bg-gradient-to-tr from-black/20 to-neutral-100/50 bg-clip-text bottom-4 left-4 max-lg:text-base ">{lastMessageContent}</p>}
       </BoxCard>
       <h4 className={"font-semibold text-xl leading-7 p-2"}>{title}</h4>
     </Link>
