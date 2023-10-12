c24751fc4b55c3ad19c87f4191a9a1fdbdf56aa4
Yash Sehgal <yashsehgal.work@gmail.com>
2023-10-12 06:09:39 +0530

    style: update text size in recent q/a session cards

--- components/ui/recent-session.tsx
+++ components/ui/recent-session.tsx
@@ -11,13 +11,14 @@ const RecentSession: React.FunctionComponent<QNASessionCardInterface> = ({
   return (
     <Link href="#" className="w-fit focus:outline-none focus:ring h-fit rounded-2xl max-sm:rounded-xl hover:scale-95 active:scale-90 transition-all focus:ring-offset-2 focus:ring-neutral-300">
       <BoxCard
-        className={cn("bg-neutral-100 hover:brightness-90"
-        )}
+        className={cn("bg-neutral-100 hover:brightness-90")}
       >
         <MessageSquare className={cn("absolute icon-wrapper top-4 right-4 text-black")} />
-        {<p className="absolute font-medium text-lg text-transparent bg-gradient-to-tr from-black/20 to-neutral-100/50 bg-clip-text bottom-4 left-4 max-lg:text-base ">{lastMessageContent}</p>}
+        {<p className="absolute font-medium text-base text-transparent bg-gradient-to-tr from-black/20 to-neutral-100/50 bg-clip-text bottom-4 left-4 max-lg:text-sm">
+          {"..."}<br />{lastMessageContent}
+        </p>}
       </BoxCard>
-      <h4 className={"font-semibold text-xl leading-7 p-2"}>{title}</h4>
+      <h4 className={"font-semibold text-base leading-snug p-2"}>{title}</h4>
     </Link>
   )
 }
