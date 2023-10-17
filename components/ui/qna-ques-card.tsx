ec2bfb5b3731ec60a37f12888823c6bffe09b39c
Shristi Gupta <shristiguptawork@gmail.com>
2023-10-18 02:39:04 +0530

    fix: Link tab removed

--- components/ui/qna-ques-card.tsx
+++ components/ui/qna-ques-card.tsx
@@ -4,13 +4,11 @@ import { cn } from "@/lib/utils";
 
 const QnaQuesCard: React.FunctionComponent<QnaQueCardInterface> = ({question, questionId, score})=>{
     return(
-        <Link href="#">
-            <BoxCard className={cn('bg-neutral-200 hover:brightness-90 flex flex-col justify-between p-4')}>
-            <div className="font-semibold text-lg">{question}</div>
-            <div className="
-            text-neutral-400">+{score} score</div>
-            </BoxCard>
-        </Link>
+        <BoxCard className={cn('bg-neutral-200 hover:brightness-90 flex flex-col justify-between p-4')}>
+        <div className="font-semibold text-lg">{question}</div>
+        <div className="
+        text-neutral-400">+{score} score</div>
+        </BoxCard>
     )
 }
 
