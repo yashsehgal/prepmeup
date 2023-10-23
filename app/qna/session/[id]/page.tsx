156b216e5bd640fd1c91c7f1a511649a014ba8e3
Yash Sehgal <yashsehgal.work@gmail.com>
2023-10-23 18:01:08 +0530

    fix: minor style improvements & folder structure

--- app/qna/session/[id]/page.tsx
+++ app/qna/session/[id]/page.tsx
@@ -65,7 +65,7 @@ const QNASession: React.FunctionComponent = () => {
                             <h2 className="question text-3xl font-semibold">{qData[qNum].question}</h2>
                             <div className="answer-wrapper ">
                                 <p className="font-medium mb-1 text-sm" >Write your answer here</p>
-                                <Textarea className="answer-box min-h-[300px] w-full px-4 py-2 placeholder:text-sm text-lg" placeholder="Good luck! You will do well" onChange={handleAnsChange} value={storeAns[qNum]} />
+                                <Textarea className="answer-box min-h-[300px] w-full px-4 py-2 placeholder:text-base text-base" placeholder="Good luck! You will do well" onChange={handleAnsChange} value={storeAns[qNum]} />
                             </div>
                         </div>
                         <Button className="mark-complete-btn mt-3 flex gap-1"><Check /><span>Mark as completed</span></Button>
