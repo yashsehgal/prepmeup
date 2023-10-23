9dd0f586414a94f596eaf552a92981eb927b26f3
Shristi Gupta <shristiguptawork@gmail.com>
2023-10-24 02:35:33 +0530

    fix: mock data fix

--- mocks/qna-questions-mock.ts
+++ mocks/qna-questions-mock.ts
@@ -4,52 +4,62 @@ const QnaMockQuestions: Array<QnaQueCardInterface> = [
     {
         question: "Explain the difference between <div> and <span> elements.",
         score: 10,
-        questionId: 0
+        questionId: 0,
+        setQuestionNum: {}
     },
     {
         question: "How do you center an element both horizontally and vertically using CSS?",
         score: 10,
-        questionId: 1
+        questionId: 1,
+        setQuestionNum: {}
     },
     {
         question: "What is the DOM (Document Object Model) in JavaScript?",
         score: 10,
-        questionId: 2
+        questionId: 2,
+        setQuestionNum: {}
     },
     {
         question: "How do you create a responsive webpage, and what are media queries?",
         score: 10,
-        questionId: 3
+        questionId: 3,
+        setQuestionNum: {}
     },
     {
         question: "What is virtual DOM, and how does it work in React?",
         score: 10,
-        questionId: 4
+        questionId: 4,
+        setQuestionNum: {}
     },
     {
         question: "How do you use Git for version control, and what are some common Git commands?",
         score: 10,
-        questionId: 5
+        questionId: 5,
+        setQuestionNum: {}
     },
     {
         question: "Explain lazy loading and its benefits for web performance.",
         score: 10,
-        questionId: 6
+        questionId: 6,
+        setQuestionNum: {}
     },
     {
         question: "How do you ensure a website is compatible with various web browsers?",
         score: 10,
-        questionId: 7
+        questionId: 7,
+        setQuestionNum: {}
     },
     {
         question: "What is the purpose of build tools like Webpack or Grunt?",
         score: 10,
-        questionId: 8
+        questionId: 8,
+        setQuestionNum: {}
     },
     {
         question: "What is unit testing, and how can you perform it in JavaScript?",
         score: 10,
-        questionId: 9
+        questionId: 9,
+        setQuestionNum: {}
     }
 ]
 
