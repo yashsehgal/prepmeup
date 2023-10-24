84601a44b5480f483c0a77bfce211b04bdca1f26
Shristi Gupta <shristiguptawork@gmail.com>
2023-10-24 06:01:20 +0530

    feat: dialog box ui completed

--- components/ui/qna-end-session-dialog.tsx
+++ components/ui/qna-end-session-dialog.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React from "react";
-import { Dialog, DialogTrigger, DialogContent, DialogHeader } from "./dialog";
+import { Dialog, DialogTrigger, DialogContent, DialogHeader, DialogTitle, DialogDescription } from "./dialog";
 import { Button } from "./button";
 
 interface QnaEndSessionDialogProps {
@@ -13,11 +13,20 @@ const QnaEndSessionDialog: React.FunctionComponent<QnaEndSessionDialogProps> = (
   return (
     <>
       <Dialog>
-        <DialogTrigger>
+        <DialogTrigger asChild>
           <Button variant={props.btnText==="End Session"? "destructive": "default"} className={props.classname}>{props.btnText}</Button>
-        </DialogTrigger>
+        </DialogTrigger >
         <DialogContent>
-          <DialogHeader></DialogHeader>
+          <DialogHeader className="p-2">
+            <DialogTitle className="mb-1">Are you sure about {props.btnText==="End Session"?"ending": "submitting"}?</DialogTitle>
+            <DialogDescription >
+                <p className="text-neutral-400 text-base mb-4">This action will end this test and will be considered as your final submission to this Q/A Session!</p>
+                <div className="flex justify-end gap-2 ">
+                    <Button variant="outline" className="text-base text-stale-900 font-base">Ah, let me try more</Button>
+                    <Button className="text-base font-base">Sure, Enought now</Button>
+                </div>
+            </DialogDescription>
+          </DialogHeader>
         </DialogContent>
       </Dialog>
     </>
